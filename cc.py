11f1579938b803e38ab15f2d9144bd15acfb537e
yehuda7 <or57570@gmail.com>
2021-01-02 14:08:33 -0800

    Counting works!

--- cc.py
+++ cc.py
@@ -12,6 +12,24 @@ def get_num_decks():
 def count_cards(num_decks):
     running_count = 0
 
-    true_count = 0
+    #the true count is the running count / number of decks
+    true_count = 0 
+    
+    not_done = True
 
-print(get_num_decks())
+    while not_done:
+        inp = input("(high: h, low: l):  ")
+
+        if(inp == "h"):
+            running_count -= 1
+        elif(inp == "l"):
+            running_count += 1
+        else:
+            not_done = False
+
+        true_count = running_count/num_decks
+        print("Running count : " + str(running_count))
+        print("True count : " + str(true_count))
+
+
+count_cards(get_num_decks())
